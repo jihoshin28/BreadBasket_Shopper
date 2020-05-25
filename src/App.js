4ca47a90cba4397915627d9735460c62426430d1
Allen Shin <geeuho@gmail.com>
2020-05-24 22:38:32 -0700

    servcies folder and api file added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom"
 import './App.css';
+import NavBar from './containers/NavBar'
+import About from './containers/About'
+import Works from './containers/Works'
+import Contact from './containers/Contact'
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div><h1>Allen Shin</h1></div>
+        <Router>
+          <NavBar/>
+          <Route exact path = '/' render = {(props)=> <About {...props}/>}/>
+          <Route exact path = '/works' render = {(props)=> <Works {...props}/>}/>
+          <Route exact path = '/contact' render = {(props)=> <Contact {...props}/>}/>
+        </Router>
+        
       </header>
+      <footer className = "footer">
+        Allen Shin 2020
+        <br></br>
+        <br></br>
+      </footer>
     </div>
   );
 }
