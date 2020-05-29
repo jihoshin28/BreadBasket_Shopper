c28a1caaeb845f8d4f0c4c2ee87486b0f20d0e5a
geeuho <geeuho@gmail.com>
2020-05-28 18:18:32 -0700

    test windows commit 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       
       <header className="App-header">
-        <div><h1>Allen Shin</h1></div>
+        <div><h1>Bread Basket Shopper</h1></div>
         <Router>
           <NavBar/>
           <Route exact path = '/' render = {(props)=> <About {...props}/>}/>
@@ -25,7 +25,7 @@ function App() {
         
       </header>
       <footer className = "footer">
-       
+      
         <br></br>
         <br></br>
       </footer>
