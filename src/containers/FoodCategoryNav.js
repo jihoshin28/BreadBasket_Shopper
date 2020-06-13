e036d7cb5dc78d9087a4831618cdaac91019b359
geeuho <geeuho@gmail.com>
2020-06-12 17:13:40 -0700

    staged changes

--- src/containers/FoodCategoryNav.js
+++ src/containers/FoodCategoryNav.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Category from '../components/Category'
 
 const FoodCategories = props => {
--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -13,33 +13,29 @@ export class OrderPage extends Component {
     }
     render() {
         return (
-                <div>
-                    
-                    <h1>Order Page</h1>
-                    <div class = "row">
-                        <div class="col-sm-4">
-                            <Dropdown />
-                        </div>
-                        <div class="col-sm-4">
-                            <h3>Grocery store name</h3>
-                        </div>
-                        <div class="col-sm-4">
-                            <Searchbar />
-                        </div>
+            <div>
+                <h1>Order Page</h1>
+                <div class = "row">
+                    <div class="col-sm-4">
+                        <Dropdown />
                     </div>
-                    <FoodCategoryNav categories = {this.state.categories}/>
-                    <br></br><br></br>
-                    {
-                        this.state.categories.map(category => {
-                            return (
-                                <FoodList name={category} />
-                            )   
-                        })
-                    }
-
+                    <div class="col-sm-4">
+                        <h3>Grocery store name</h3>
+                    </div>
+                    <div class="col-sm-4">
+                        <Searchbar />
+                    </div>
                 </div>
-           
-            
+                <FoodCategoryNav categories = {this.state.categories}/>
+                <br></br><br></br>
+                {
+                    this.state.categories.map(category => {
+                        return (
+                            <FoodList name={category} />
+                        )   
+                    })
+                }
+            </div>
         )
     }
 }
