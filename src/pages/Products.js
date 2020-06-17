8a5c8c28285a1d945e0a9b4cd5bb32a112d98854
geeuho <geeuho@gmail.com>
2020-06-17 15:09:43 -0700

    added products page and sidebar

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -7,48 +7,55 @@ class Products extends Component {
             foodCategory: props
         }
     }
+
+    formSubmit = (event) => {
+        event.preventDefault()
+        console.log(event.value)
+    } 
+
     render(){
         return (
             <div class = "wrapper">
                 <div id = "sidebar">
                     <div class = "sidebar-header">
-                        <h3>Make a Selection</h3>
+                        <h3>Categories</h3>
                     </div>
-                    <ul class = "list-unstyled components">
-                        <p>Categories</p>
+                    <form onSubmit = {this.formSubmit}>
                         
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="Meat/Seafood Input"/> Meat/Seafood
-                        </div>
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="Produce Input" /> Produce
-                        </div>
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="Dairy Input" /> Dairy
-                        </div>
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="Snacks Input" /> Snacks
-                        </div>
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="Beverages Input" /> Beverages
-                        </div>
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="Frozen Input" /> Frozen
-                        </div>
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="Household Input" /> Household
-                        </div>
-                        <div class="input-group-text">
-                            <input type="radio" name= "food-category" aria-label="More Input" /> More
-                        </div>
-                    </ul>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="meats" aria-label="Meat/Seafood Input"/> Meat/Seafood
+                            </div>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="produce" aria-label="Produce Input"/> Produce
+                            </div>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="dairy" aria-label="Dairy Input"/> Dairy
+                            </div>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="snacks" aria-label="Snacks Input"/> Snacks
+                            </div>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="beverage" aria-label="Beverages Input"/> Beverages
+                            </div>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="frozen" aria-label="Frozen Input"/> Frozen
+                            </div>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="household" aria-label="Household Input"/> Household
+                            </div>
+                            <div class="input-group-text">
+                                <input type="radio" name= "food-category" value="more" aria-label="More Input"/> More
+                            </div>
+                      
+                        <button type="submit">Search</button>
+                    </form>
+                    
                 </div>
 
-                <div id = "content container"> 
+                <div id = "content"> 
                     <div class = "row justify-content-center">
                         <h1> Hello</h1>
                     </div>
-                    
                 </div>
 
             </div>
