f7242cad472294402452af283eeb78a3245913c3
geeuho <geeuho@gmail.com>
2020-06-26 15:53:47 -0700

    Order History page editted

---
+++ src/components/OrderHistoryItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+class OrderHistoryItem extends Component {
+    render(){
+        return (
+            <div class ="row">
+                <div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default OrderHistoryItem
