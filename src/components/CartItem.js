8e896506b90d80b13befc44b0b73bd80d9428181
geeuho <geeuho@gmail.com>
2020-06-29 23:54:24 -0700

    cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,56 @@
 import React, {Component} from 'react'
 
 class CartItem extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count: props.count,
+            price: (props.price * props.count).toFixed(2)
+        }
+    }
+
+    handleClick = (event) => {
+        if(event.target.innerHTML === '+'){
+            this.setState({
+                count: this.state.count + 1,
+                price: (this.props.price * (this.state.count + 1)).toFixed(2)
+            })
+        } else if(event.target.innerHTML === '-') {
+            if(this.state.count > 0){
+                this.setState({
+                    count: this.state.count - 1,
+                    price: (this.props.price * (this.state.count - 1)).toFixed(2)
+                })
+            }
+        }
+    }
     render(){
+        
         return(
             <div class = "row cartItem">
                 <div>
                     <h3>{this.props.name}</h3>
                     <img width = "200px" src={this.props.image}/>
-                    
                 </div>
                 <div>
-                    <h3>Price: $13.40</h3>
-                    <h3></h3>
+                    <h3>Price per unit: ${this.state.price}</h3>
+                    <div>
+                        <h3>Count: {this.state.count}</h3>
+                        <span>
+                            <button onClick= {this.handleClick}>
+                                +
+                            </button>
+                            <button onClick = {this.handleClick}>
+                                -
+                            </button>
+                        </span>
+                        
+                    </div>
+                    <h3>Total: ${this.state.price} </h3>
+                </div>
+                <div>
+                    <button>Remove Item</button>
+                    <button>Item Details</button>
                 </div>
             </div>
         )
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,23 +8,33 @@ class Cart extends Component{
             items: [
                 {
                     image: "https://media.istockphoto.com/photos/tomato-isolated-on-white-background-picture-id466175630?k=6&m=466175630&s=612x612&w=0&h=fu_mQBjGJZIliOWwCR0Vf2myRvKWyQDsymxEIi8tZ38=",
-                    name: "Tomato"
+                    name: "Tomato",
+                    count: 1,
+                    price: .75
                 },
                 {
                     image: "https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-                    name: "Bananas"
+                    name: "Bananas",
+                    count: 5,
+                    price: .50
                 },
                 {
                     image: "https://www.sprouts.com/wp-content/uploads/2017/11/Sweet-Potatoes.jpg",
-                    name: "Sweet Potatoes"
+                    name: "Sweet Potatoes",
+                    count: 9,
+                    price: .25
                 },
                 {
                     image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2019%2F10%2Fdozen-eggs-in-carton.png",
-                    name: "Eggs"
+                    name: "Eggs",
+                    count: 1,
+                    price: 3.50
                 },
                 {
                     image: "https://www.kingarthurflour.com/sites/default/files/styles/featured_image/public/2020-02/the-easiest-loaf-of-bread-youll-ever-bake.jpg?itok=j89yDeId",
-                    name: "Bread"
+                    name: "Bread",
+                    count: 1,
+                    price: 2.00
                 }
             ]
         }
@@ -37,7 +47,7 @@ class Cart extends Component{
                 <div class = 'wrapper cart'>
                     <h1>Cart</h1>
                     {this.state.items.map(item => {
-                        return <CartItem image = {item.image} name = {item.name}/>
+                        return <CartItem image = {item.image} name = {item.name} count = {item.count} price = {item.price}/>
                     })}
                 </div>
                 
