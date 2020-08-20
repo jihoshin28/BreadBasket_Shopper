81abea4a8fb7487547c183985e260ca32a96f072
geeuho <geeuho@gmail.com>
2020-08-20 13:48:15 -0700

    added all get functions for API calls on client with skeleton

--- src/services/Api.js
+++ src/services/Api.js
@@ -6,19 +6,94 @@ const headers = {
     "Authorization": token
 }
 
+const shopperSignUp = async () => {
 
+}
+
+const getShopper = async () => {
+
+}
+
+const driverSignUp = async () => {
+
+}
+
+const getDriver = async () => {
+
+}
+
+const getStores = async () => {
+
+}
+
+const getStore = async () => {
+
+}
+
+const getOpenOrders = async () => {
+
+}
+
+const getActiveOrders = async () => {
+
+}
+
+const getStoreOrders = async () => {
+
+}
+
+const getShopperOrders = async () => {
+
+}
+
+const getItems = async () => {
+    let response = await fetch(API_ROOT + '/items', {
+        method: 'GET',
+        headers: headers
+    })
+    return response.json()
+}
+
+const getItemsCategory = async () => {
+
+}
+
+const getCartItemsCategory = async () => {
+
+}
+
+const getCartItemsOrder = async () => {
+
+}
+
+const getCartItemsActive = async () => {
+
+}
+
+const getCartItemsDone = async () => {
+
+}
+
+const getDriverOrder = async () => {
+
+}
 
 export default {
-    getUser,
-    getStores,
-    getStore,
     shopperSignUp,
     getShopper,
+    driverSignUp,
+    getDriver,
+    getStores,
+    getStore,
     getOpenOrders,
+    getActiveOrders,
     getStoreOrders,
-    getShopperOrder,
-    
-    
-    getItems
+    getShopperOrders,
+    getItems,
+    getItemsCategory,
+    getCartItemsOrder,
+    getCartItemsActive,
+    getCartItemsDone,
+    getDriverOrder
 
 }
