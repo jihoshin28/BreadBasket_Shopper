2cb88136cc2989b5ceffd84e651621afbdea7a0d
geeuho <geeuho@gmail.com>
2020-09-09 22:03:51 -0700

    current orders backbone

--- src/pages/CurrentOrders.js
+++ src/pages/CurrentOrders.js
@@ -2,10 +2,26 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class CurrentOrder extends React.Component {
+    componentDidMount(){
+        console.log(this.props.activeOrders)
+        console.log(this.props.activeOrders.map(order => order.id))
+    }
+
+    renderOrders = () => {
+        return this.props.activeOrders.map(order => {
+            return (
+                <h1> {order}</h1>
+            )   
+        })
+    }
+
     render(){
         return(
             <div className = "container">
                 <h1>Current Order</h1>
+                <div>
+                    {this.renderOrders()}
+                </div>
             </div>
         )
     }
@@ -13,7 +29,7 @@ class CurrentOrder extends React.Component {
 
 let mapStateToProps = state => {
     return ({
-        
+        activeOrders: state.order.active_order_ids
     })
 }
 
