f87d4ef7025c2bb853ee527691dce4fcbc81e74b
geeuho <geeuho@gmail.com>
2020-10-06 20:07:50 -0700

    add build folder

---
+++ build/precache-manifest.db2101f56d9bdd6267baae447225e74c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4c13826d2667aa3a53c9a850d5c6d9",
+    "url": "/git@github.com:geeuho/BreadBasket_Backend.git/index.html"
+  },
+  {
+    "revision": "a5178e8bbba0b046fb0a",
+    "url": "/git@github.com:geeuho/BreadBasket_Backend.git/static/css/main.5cf99db4.chunk.css"
+  },
+  {
+    "revision": "68dfa59e2c4db5e123e5",
+    "url": "/git@github.com:geeuho/BreadBasket_Backend.git/static/js/2.e8082fe4.chunk.js"
+  },
+  {
+    "revision": "88cac6a8736423293e8ef4662918ec16",
+    "url": "/git@github.com:geeuho/BreadBasket_Backend.git/static/js/2.e8082fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5178e8bbba0b046fb0a",
+    "url": "/git@github.com:geeuho/BreadBasket_Backend.git/static/js/main.a97ec460.chunk.js"
+  },
+  {
+    "revision": "2c4be8a6aaa926a25bae",
+    "url": "/git@github.com:geeuho/BreadBasket_Backend.git/static/js/runtime-main.8c1a0f37.js"
+  }
+]);
