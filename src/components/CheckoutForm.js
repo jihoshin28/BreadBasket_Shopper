10b6c40ec75871b487393073f1e865b29d19d739
geeuho <geeuho@gmail.com>
2020-10-09 17:50:35 -0700

    submit stripe form fxn

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -3,7 +3,25 @@ import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js'
 const CheckoutForm = () => {
     const stripe = useStripe()
     const elements = useElements()
-     
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if(!stripe || !elements){
+            return
+        }
+
+        const cardElement = elements.getElement(CardElement)
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card: cardElement
+        });
+
+        if(error) {
+            console.log('error', error)
+        } else {
+            console.log('payment', paymentMethod)
+        }
+    }
     return(
         <CardElement/>
     )
