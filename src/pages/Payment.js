d2c4cd8fd24782e9163af3239211d77771e27a60
geeuho <geeuho@gmail.com>
2020-10-13 22:01:57 -0700

    basic card for item

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -3,12 +3,8 @@ import {connect} from 'react-redux'
 
 import PaymentForm from '../components/PaymentForm'
 import StripeCheckout from 'react-stripe-checkout'
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
 import { processOrder } from '../actions'
 import OrderItem from '..'
-
-const stripePromise = loadStripe('')
 
 class Payment extends React.Component {
     componentDidMount(){
@@ -53,9 +49,18 @@ class Payment extends React.Component {
         console.log(cartItems)
         return cartItems.map(item => {
             console.log(item.attributes)
-            return <div class = "row">
+            return <div class = "row checkout-item">
+                        <div class = "card" style = {{height: "20%", width: "20%"}}>
+                            <img src={item.attributes.item.image} style={{ height: "100%" }} class="card-img-top" alt="..." />
+                        </div>
                     <h3>{item.attributes.item.name}</h3>
-                    <h3>{item.attributes.quantity_num}</h3>
+                    <div class = "row">
+                        <h3>+</h3>
+                        <h3>
+                            {item.attributes.quantity_num}
+                        </h3>
+                        <h3>-</h3>
+                    </div>
                     <h3>{`$${(item.attributes.item.price * .01 * item.attributes.quantity_num).toFixed(2)}`}</h3>
                 </div>
             
@@ -71,7 +76,7 @@ class Payment extends React.Component {
                     <div class="wrapper"> 
                             <div className = "row payment-box">
                                 <div class= "col-6 payment-items">
-                                    <h2 class = "payment-header">Checkout</h2>
+                                    <h1 class = "payment-header">Checkout</h1>
                                     <div class = "row">
                                         <p>Items</p>
                                         <p>Quantity</p>
@@ -87,11 +92,9 @@ class Payment extends React.Component {
                             
                                 <div class="col-6 payment-total">
                                     <StripeCheckout
-                                        onClick = {this.renderItems}
                                         token = {this.onToken}
                                         stripeKey= "pk_test_51HN5XFKYkELgOBXmFpEJqnw7WynOS5irzHdnuse7CMysCArWYZPwclIdO73m8Ot8CVNn6pQANPfuPkbDmLk3HRdD00ss20lGUO"
                                     >
-
                                     </StripeCheckout>
                                 </div>
 
