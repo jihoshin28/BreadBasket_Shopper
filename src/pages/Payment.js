a94831e7f3e95eeec48bed90233bd753c35d34b3
geeuho <geeuho@gmail.com>
2020-10-13 20:56:04 -0700

    added data for checkout on payment page

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -6,6 +6,7 @@ import StripeCheckout from 'react-stripe-checkout'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import { processOrder } from '../actions'
+import OrderItem from '..'
 
 const stripePromise = loadStripe('')
 
@@ -14,6 +15,7 @@ class Payment extends React.Component {
         console.log(process.env.STRIPE_TEST_KEY)
         console.log(this.props.cartItems)
     }
+
     constructor() {
         super()
         this.state = {
@@ -41,12 +43,23 @@ class Payment extends React.Component {
         await this.props.processOrder(cartItems, this.props.cart_id, this.props.currentOrderId, { status: "active" })
     }
 
-    stripePay = () => {
-        this.testFunction()
+    onToken = () => {
+
     }
 
-    renderItems = ()  => {
-        
+    renderItems(){
+        let keys = Object.keys(this.props.cartItems)
+        let cartItems = keys.map(key => this.props.cartItems[key])
+        console.log(cartItems)
+        return cartItems.map(item => {
+            console.log(item.attributes)
+            return <div class = "row">
+                    <h3>{item.attributes.item.name}</h3>
+                    <h3>{item.attributes.quantity_num}</h3>
+                    <h3>{`$${(item.attributes.item.price * .01 * item.attributes.quantity_num).toFixed(2)}`}</h3>
+                </div>
+            
+        }) 
     }
 
 
@@ -59,14 +72,24 @@ class Payment extends React.Component {
                             <div className = "row payment-box">
                                 <div class= "col-6 payment-items">
                                     <h2 class = "payment-header">Checkout</h2>
+                                    <div class = "row">
+                                        <p>Items</p>
+                                        <p>Quantity</p>
+                                        <p>Price</p>
+                                    </div>
+                                    <div class = "payment-divider">
+
+                                    </div>
                                     <div class = "checkout-items">
-                                        hello
+                                        {this.renderItems()}
                                     </div> 
                                 </div>
                             
                                 <div class="col-6 payment-total">
                                     <StripeCheckout
-                                    stripeKey= "pk_test_51HN5XFKYkELgOBXmFpEJqnw7WynOS5irzHdnuse7CMysCArWYZPwclIdO73m8Ot8CVNn6pQANPfuPkbDmLk3HRdD00ss20lGUO"
+                                        onClick = {this.renderItems}
+                                        token = {this.onToken}
+                                        stripeKey= "pk_test_51HN5XFKYkELgOBXmFpEJqnw7WynOS5irzHdnuse7CMysCArWYZPwclIdO73m8Ot8CVNn6pQANPfuPkbDmLk3HRdD00ss20lGUO"
                                     >
 
                                     </StripeCheckout>
