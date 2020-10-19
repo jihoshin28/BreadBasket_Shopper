7e18d29f03d315171d1b405e10be3d430b8d19f7
geeuho <geeuho@gmail.com>
2020-10-19 15:44:03 -0700

    added paypal button

--- src/components/PayPalButton.js
+++ src/components/PayPalButton.js
@@ -1,9 +1,25 @@
+import { data } from 'jquery'
 import React from 'react'
+import {PayPalButton} from 'react-paypal-button-v2'
 
 class PayPalButton extends React.Component {
     render(){
         return(
-            
+            <PayPalButton
+                amount = ".01"
+                onSuccess = {(details, data) => {
+                    alert(details)
+                    return fetch("/paypal-transaction-complete", {
+                        method: 'post',
+                        body: JSON.stringify({
+                            orderId: data.orderID
+                        })
+                    })
+                }}
+                options ={{
+                    clientId: "Aef4WiQOrgTXuMCJ7yrTuJ75h1TfnhbaWg_XatCXfSeVb7WBCGtE0EE0Uyid5JN5NYPn27vIHT6IE8iR"
+                }}
+            ></PayPalButton>
         )
     }
 }
