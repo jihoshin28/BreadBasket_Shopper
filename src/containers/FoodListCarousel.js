ba8fd8a32e14c9c02e5a1f517b8e69eaa7192515
geeuho <geeuho@gmail.com>
2020-11-04 17:59:17 -0800

    food list centered

--- src/containers/FoodListCarousel.js
+++ src/containers/FoodListCarousel.js
@@ -1,36 +1,31 @@
 import React, { Component } from 'react';
+import FoodList from '../containers/FoodListCarousel'
 
 
 class FoodListCarousel extends Component {
     render(){
         return (
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div id="carouselExampleIndicators" class="carousel slide center food-list-carousel" data-interval="false" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
-            <div data-interval = "false" class="carousel-inner food-list-carousel">
-                <div class="carousel-item active">
-                    <div class='food-list-section'>
+            <div class="carousel-inner">
+                    <div class="carousel-item active food-list-section">
                             <h1>
                                 Slide 1
                             </h1>
-                        </div>
-                </div>
-                    <div class="carousel-item">
-                        <div class = 'food-list-section'>
-                            <h1>
-                                Slide 2
-                            </h1>
-                        </div>
                     </div>
-                    <div class="carousel-item">
-                        <div class='food-list-section'>
-                            <h1>
-                                Slide 3
-                            </h1>
-                        </div>
+                    <div class="carousel-item food-list-section">
+                        <h1>
+                            Slide 2
+                        </h1>
+                    </div>
+                    <div class="carousel-item food-list-section">
+                        <h1>
+                            Slide 3
+                        </h1>
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
