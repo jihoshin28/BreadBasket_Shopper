2c70eb09b5385824eecaeaf7c7dfd9bc8583ec47
geeuho <geeuho@gmail.com>
2020-11-04 16:48:11 -0800

    infinite carousel downloaded

--- src/containers/FoodListCarousel.js
+++ src/containers/FoodListCarousel.js
@@ -1,36 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Slider from 'infinite-react-carousel';
 
-class FoodListCarousel extends React.Component {
-    render(){
-        return(
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="..." class="d-block w-100" alt="..." />
+class FoodListCarousel extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            duration: 300,
+            initialSlide: 1,
+            swipe: false
+        };
+        return (
+            <div>
+                <span>CustomSlider</span>
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
                     </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="..." />
+                    <div>
+                        <h3>2</h3>
                     </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="..." />
+                    <div>
+                        <h3>3</h3>
                     </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                </Slider>
             </div>
-        )
+        );
     }
 }
-
 export default FoodListCarousel
