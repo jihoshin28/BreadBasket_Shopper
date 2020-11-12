dec821ad4eb6ab686c2746ecfa9865d2cdca46dd
geeuho <geeuho@gmail.com>
2020-11-11 17:47:25 -0800

    editted categories

--- src/categories.js
+++ src/categories.js
@@ -124,7 +124,7 @@ export default [
         },
         {
             title: "Meat/Seafood",
-            name: "meats"
+            name: "meatnseafood"
         },
         {
             title: "Snacks",
@@ -158,7 +158,7 @@ export default [
         },
         {
             title: "Meat/Seafood",
-            name: "meats"
+            name: "meatnseafood"
         },
         {
             title: "Produce",
