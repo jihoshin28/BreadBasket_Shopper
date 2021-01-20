748001e55649f9291d23363104ef4b956838cdc1
Allen <geeuho@gmail.com>
2021-01-19 19:44:11 -0800

    stripe checkout page functional

--- src/actions/index.js
+++ src/actions/index.js
@@ -295,7 +295,7 @@ export const stripePayment = (event, stripePromise) => async dispatch => {
     
     const session = await response.data
     
-    // debugger;
+    debugger;
     const result = await stripe.redirectToCheckout({
         sessionId: session.id
     })
--- src/services/Rails.js
+++ src/services/Rails.js
@@ -11,7 +11,7 @@ const headers = {
 }
 
 export default axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://bread-basket-backend.herokuapp.com',
     headers: headers 
 })
 
