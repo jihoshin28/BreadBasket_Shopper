dab04fe1541e7ba8e7351db3ba99c9068fcc36f8
Allen <jihoshin.28@gmail.com>
2021-02-03 23:22:01 -0800

    added button with toggle bottom edit

--- src/components/PaymentOption.js
+++ src/components/PaymentOption.js
@@ -1,10 +1,50 @@
 import React from 'react' 
+import { Link } from 'react-router-dom'
 
 class PaymentOption extends React.Component{
-    constructor(){
+    constructor(props){
         super()
         this.state = {
+            selectType: props.selectType,
+            showBottomEdit: false
+        }
+    }
 
+    showBottomEdit = (showState)  => {
+        if(!showState){
+            this.setState({
+                showBottomEdit: true
+            })
+        } else {
+            this.setState({
+                showBottomEdit: false
+            })
+        }
+    }
+
+    renderSelect = (type) => {
+        if(type === 'none'){
+            return (
+                <div></div>
+            )
+        } else if (type === "add"){
+            return (
+                <Link onClick = {() => this.showBottomEdit(this.state.showBottomEdit)}>
+                    Add
+                </Link>
+            )
+        } else if(type === "select"){
+            return(
+                <Link onClick = {() => this.showBottomEdit(this.state.showBottomEdit)}>
+                    Select
+                </Link>
+            )
+        } else if(type === "edit"){
+            return(
+                <Link onClick = {() => this.showBottomEdit(this.state.showBottomEdit)}>
+                    Edit
+                </Link>
+            )
         }
     }
 
@@ -18,15 +58,20 @@ class PaymentOption extends React.Component{
                             </h3>
                         </div>
                         <div>
-                            <a href = "#">
-                                Link
-                            </a>
+                            {this.renderSelect(this.state.selectType)}
                         </div>
                     </div>
                     <div className = "payment-divider">
                     </div>
-                    <div>
-            
+                    <div className = "row payment-option-line">
+                        {this.state.showBottomEdit ? 
+                            this.props.bottomContent
+                            :
+                            <div>
+                                
+                            </div>
+
+                        }
                     </div>
             </div>
         )
--- src/containers/PaymentOptions.js
+++ src/containers/PaymentOptions.js
@@ -10,7 +10,9 @@ class PaymentOptions extends React.Component{
     //Contact Information
     //Conditionally Add if no contacts
     //Select Button w/ Add Line or Phone Number Display underneath
-    
+    contactInfo = () => {
+
+    }
 
     //Delivery Address
     //Conditionally Add if no address
@@ -33,13 +35,13 @@ class PaymentOptions extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <PaymentOption title = 'Phone Number'/>
-                <PaymentOption title = 'Address'/>
-                <PaymentOption title = 'Tip'/>
-                <PaymentOption title = 'Delivery Time'/>
-                <PaymentOption title = 'Substitution Preference'/>
-                <PaymentOption title = 'Delivery Notes'/>
-                <PaymentOption title = 'Payment Method'/>
+                <PaymentOption title = 'Phone Number' selectType = "add" bottomContent = {<h1>Hello</h1>}/>
+                <PaymentOption title = 'Address' selectType = "add"/>
+                <PaymentOption title = 'Tip' selectType = "select" />
+                <PaymentOption title = 'Delivery Time' selectType = "select"/>
+                <PaymentOption title = 'Substitution Preference' selectType = "select"/>
+                <PaymentOption title = 'Delivery Notes' selectType = "edit"/>
+                <PaymentOption title = 'Payment Method' selectType = "none" bottomContent = {<h1>Hello</h1>}/>
             </React.Fragment>
           
 
