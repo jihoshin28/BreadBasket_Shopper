02b35c90d0b30bc7e4561edcf00eff7f1e13b274
Allen <jihoshin.28@gmail.com>
2021-02-19 12:47:13 -0800

    Updates

---
+++ precache-manifest.02614ecbf49163b5a2b99dbc6092f0f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7938cb666f9342f9b06737ccedae43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e8ba1da50ce7f7485c1",
+    "url": "/static/css/2.83c8ed2b.chunk.css"
+  },
+  {
+    "revision": "0ee5c380be9dcdc66a1f",
+    "url": "/static/css/main.3db36155.chunk.css"
+  },
+  {
+    "revision": "0e8ba1da50ce7f7485c1",
+    "url": "/static/js/2.e760cd61.chunk.js"
+  },
+  {
+    "revision": "a08613626309ee51fce37f6dc8b109ef",
+    "url": "/static/js/2.e760cd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee5c380be9dcdc66a1f",
+    "url": "/static/js/main.a330c796.chunk.js"
+  },
+  {
+    "revision": "bd82f80c967998e101ce",
+    "url": "/static/js/runtime-main.2e687ccd.js"
+  }
+]);
