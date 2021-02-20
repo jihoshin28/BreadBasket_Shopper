6077fe9f09bee02b2f479b8d88fafada88f704ca
Allen <jihoshin.28@gmail.com>
2021-02-19 22:36:18 -0800

    using has router

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -18,7 +18,7 @@ export class Navbar extends Component {
             return (
                 <div>
                     <a class="nav-link">
-                        <Link to={process.env.PUBLIC_URL + '/profile'}>
+                        <Link to='/profile'>
                             <img class="google-pic" alt="alt img" src={this.props.userAuthPic} />
                         </Link>
                     </a>
@@ -43,37 +43,37 @@ export class Navbar extends Component {
                     <span class="navbar-toggler-icon"></span>
                 </button> */}
                 <div class="collapse navbar-collapse " id="navbarNav">
-                    <Link class='header-logo' to={process.env.PUBLIC_URL + '/'}>
-                        <img class="header-icon" src={process.env.PUBLIC_URL + '/bread_basket_icon.jpg'}></img>
+                    <Link class='header-logo' to={'/'}>
+                        <img class="header-icon" src={'/bread_basket_icon.jpg'}></img>
 
                     </Link>
                     {(!!this.props.signedIn) ?
                     
                     <ul class="navbar-nav">
                         <li class="nav-item nav-list active">
-                            <a href = "/orderpage" class="menu-button">
+                            <Link to = "/orderpage" class="menu-button">
                                 Order
-                            </a>
+                            </Link>
                         </li>
                             
                         {(!isEmpty(this.props.activeOrders)) ? 
                                 <li class="nav-item nav-list">
-                                    <a href = "/active_orders" class="menu-button">Active Orders</a>
+                                    <Link to = "/active_orders" class="menu-button">Active Orders</Link>
                                 </li>
                                 :
                                 null
                         }
                         <li class="nav-item nav-list">
-                            <a href= {process.env.PUBLIC_URL + "/order_history"} class="menu-button">Order History</a>
+                            <Link to = "/order_history" class="menu-button">Order History</Link>
                         </li>
                         <li class="nav-item nav-list">
-                            <a href = {process.env.PUBLIC_URL + '/about'} class="menu-button">About</a>
+                            <Link to = '/about' class="menu-button">About</Link>
                         </li>
                     </ul>
                     :
                     <ul class="navbar-nav">
                         <li class="nav-item nav-list">
-                            <a href = {process.env.PUBLIC_URL + "/about"} class="menu-button">About</a>
+                            <Link to = "/about" class="menu-button">About</Link>
                         </li>
                     </ul>
                     }
@@ -83,7 +83,7 @@ export class Navbar extends Component {
                     <li class="nav-item">
                         <button class="btn btn-warning" type="button">
                             <NotificationBadge style = {{left: '90%', bottom:'0px', top: 'none', right: 'none'}} count = {cart_length} effect = {Effect.SCALE}/> 
-                            <Link class="text-reset" to={process.env.PUBLIC_URL + '/cart'}>
+                            <Link class="text-reset" to={'/cart'}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="currentColor">
                                     <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z" />
                                 </svg>
